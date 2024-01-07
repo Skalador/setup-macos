4b56207f3a30a97d62d01ea0867885bcf524bf7a
Skalador <k.niederwanger@gmail.com>
2024-01-07 14:29:15 +0100

    Adding homebrew_check display

--- tests/uninstall-homebrew.sh
+++ tests/uninstall-homebrew.sh
@@ -7,7 +7,7 @@ curl -sLO https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh
 chmod +x ./uninstall.sh
 sudo ./uninstall.sh --force
 
-# Clean up Homebrew directories.
+# Clean up Homebrew directories for x86_64 cpu architecture.
 sudo rm -rf /usr/local/Homebrew
 sudo rm -rf /usr/local/Caskroom
 sudo rm -rf /usr/local/bin/brew
